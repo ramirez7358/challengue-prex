ded09aba82818e8e43381a4bac7b243c2f45ec2e
Brian Ramirez <ramirez7358@gmail.com>
2024-01-25 23:21:50 -0300

    Add documentation to handles

--- src/handler.rs
+++ src/handler.rs
@@ -12,6 +12,12 @@ use crate::{
     response::{CreateClientResponse, GenericResponse, SingleResponse},
 };
 
+/// Configures the service routes for the web application.
+///
+/// This function sets up the Actix service configuration by adding various service endpoints.
+///
+/// # Arguments
+/// * `conf` - A mutable reference to the ServiceConfig to which the routes will be added.
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/app")
         .service(health_checker_handler)
@@ -24,6 +30,17 @@ pub fn config(conf: &mut web::ServiceConfig) {
     conf.service(scope);
 }
 
+/// Creates a new client.
+///
+/// This endpoint processes a request to create a new client, ensuring no duplicate clients
+/// are created based on the document number.
+///
+/// # Arguments
+/// * `body` - The JSON payload containing the client's information.
+/// * `data` - Shared application state, specifically a list of current clients.
+///
+/// # Returns
+/// * A success response with the new client's ID, or a conflict response if a duplicate is found.
 #[post("/new_client")]
 async fn new_client(body: web::Json<ClientInfo>, data: web::Data<AppState>) -> impl Responder {
     let mut vec = data.clients_temp_db.lock().unwrap();
@@ -55,6 +72,17 @@ async fn new_client(body: web::Json<ClientInfo>, data: web::Data<AppState>) -> i
     })
 }
 
+/// Processes a new credit transaction.
+///
+/// This endpoint applies a credit transaction to a client's account. It adds the specified
+/// amount to the client's balance.
+///
+/// # Arguments
+/// * `body` - JSON payload containing the transaction request details.
+/// * `data` - Shared application state, including client data.
+///
+/// # Returns
+/// * A response indicating success or failure of the transaction.
 #[post("/new_credit_transaction")]
 async fn new_credit_transaction(
     body: web::Json<CreditOrDebitRequest>,
@@ -67,6 +95,17 @@ async fn new_credit_transaction(
     .await
 }
 
+/// Processes a new debit transaction.
+///
+/// This endpoint applies a debit transaction to a client's account. It deducts the specified
+/// amount from the client's balance if sufficient funds are available.
+///
+/// # Arguments
+/// * `body` - JSON payload containing the transaction request details.
+/// * `data` - Shared application state, including client data.
+///
+/// # Returns
+/// * A response indicating success or failure of the transaction.
 #[post("new_debit_transaction")]
 async fn new_debit_transaction(
     body: web::Json<CreditOrDebitRequest>,
@@ -83,6 +122,16 @@ async fn new_debit_transaction(
     .await
 }
 
+/// Stores client balances to a file and resets them in memory.
+///
+/// This endpoint saves all client balances to a file and then resets each balance to zero.
+/// The file is named based on the current date and a count of previously stored files.
+///
+/// # Arguments
+/// * `data` - Shared application state, including client data.
+///
+/// # Returns
+/// * A success response if the balances are stored and reset, or an error response otherwise.
 #[post("store_balances")]
 async fn store_balances(data: web::Data<AppState>) -> impl Responder {
     let mut vec = data.clients_temp_db.lock().unwrap();
@@ -101,6 +150,16 @@ async fn store_balances(data: web::Data<AppState>) -> impl Responder {
     }
 }
 
+/// Retrieves the balance of a specific client.
+///
+/// This endpoint returns the current balance of a client based on their ID.
+///
+/// # Arguments
+/// * `path` - The path parameter containing the client's ID.
+/// * `data` - Shared application state, including client data.
+///
+/// # Returns
+/// * A response with the client's balance or a failure message if the client is not found.
 #[get("client_balance/{client_id}")]
 async fn client_balance(path: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     let mut vec = data.clients_temp_db.lock().unwrap();
@@ -118,6 +177,12 @@ async fn client_balance(path: web::Path<String>, data: web::Data<AppState>) -> i
     }
 }
 
+/// Health check endpoint for the web application.
+///
+/// This endpoint provides a basic health check, returning a success message.
+///
+/// # Returns
+/// * A success response with a predefined health check message.
 #[get("/healthchecker")]
 async fn health_checker_handler() -> impl Responder {
     const MESSAGE: &str = "Build Simple CRUD API with Rust and Actix Web";
@@ -129,6 +194,21 @@ async fn health_checker_handler() -> impl Responder {
     HttpResponse::Ok().json(response_json)
 }
 
+/// Generic function to process credit or debit transactions.
+///
+/// This function applies a transaction to a client's account based on the provided logic.
+/// It is used to encapsulate common logic for credit and debit operations.
+///
+/// # Type Parameters
+/// * `F`: The type of closure that defines the transaction logic.
+///
+/// # Arguments
+/// * `body` - JSON payload containing the transaction request details.
+/// * `data` - Shared application state, including client data.
+/// * `transaction_logic` - The closure that contains the specific logic for the transaction.
+///
+/// # Returns
+/// * A response indicating success or failure of the transaction.
 async fn process_transaction<F>(
     body: web::Json<CreditOrDebitRequest>,
     data: web::Data<AppState>,
